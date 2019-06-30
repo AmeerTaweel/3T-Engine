2c0d856760f4dcd654008a8590d05a4401d8929c
Ameer Taweel <ameertaweel2002@gmail.com>
2019-06-30 20:14:18 +0300

    feat: Implement generate labels

--- src/generate_labels.py
+++ src/generate_labels.py
@@ -1,3 +1,45 @@
+import time
 import numpy as np
+from utils import TerminalText as tt
+from min_max_player import predictBestNextMove
 
-g = np.load('./src/generated/dataset.npy')
+def generateLabels(N, games):
+    """
+    Compute labels (best moves) for the games
+
+    Parameters
+    ----------
+    N (Int): Number of rows/columns in board
+    games: List
+
+    Returns
+    -------
+    list: Labels for the games.
+    """
+
+    start_time = time.time() # To measure script running time
+
+    if N < 1: # N can't be less than one
+        N = 1
+    M = len(games)
+
+    labels = np.full(M, 0, np.uint8)
+
+    print("")
+    print(tt.BLUE + "Generating Labels..." + tt.END)
+    print("") # To prevent the code in the loop from erasing the "Board Size" line
+
+    for i, game in enumerate(games):
+        percentage = "%.2f" % (((i + 1) / M) * 100) # Limit to two decimal points
+        print(tt.ERASE + tt.BLUE + "MinMax Predicting Best Moves: " + tt.END + f"{percentage}%")
+        labels[i] = predictBestNextMove(game, N)
+
+    total_time = time.time() - start_time
+    running_time_in_seconds = "%.2f" % total_time # Formatted running time
+
+    print(tt.BLUE + "Generated labels" + tt.BOLD + " (best moves) " + tt.END
+        + tt.BLUE + "for " + tt.END + f"{M}" + tt.BLUE + " games in" + tt.END
+        + f" {running_time_in_seconds} " + tt.BLUE + "seconds." + tt.END)
+    print("")
+
+    return labels
