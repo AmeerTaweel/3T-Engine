2ae54c6b41de041302e56dc8bf818e0c18384c71
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-04 21:42:14 +0300

    feat: Remove the sigmoid and hypothesis function to convert to TensorFlow

--- src/hypothesis.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from sigmoid import sigmoid
-
-def h(X, Theta):
-    return np.vectorize(sigmoid)(X @ Theta)
--- src/sigmoid.py
+++
@@ -1,4 +0,0 @@
-from math import exp
-
-def sigmoid(z):
-    return 1 / (1 + exp(-z))
