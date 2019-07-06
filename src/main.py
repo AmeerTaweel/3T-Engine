60ded71d79b863cbc8bf95ac75121d95939af180
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-06 19:58:51 +0300

    feat: Add some logs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import time
 import numpy as np
 import tensorflow as tf
+from utils import TerminalText as tt
 
 # Don't run this script before generating dataset
+
+start_time = time.time() # To measure script running time
+
+print("")
+print(tt.GREEN + "Loading dataset..." + tt.END)
 
 examples = np.load('./src/generated/features.npy')
 labels = np.load('./src/generated/labels.npy')
@@ -9,14 +16,21 @@ labels = np.load('./src/generated/labels.npy')
 M = examples.shape[0] # Number of examples
 N = examples.shape[1] # Number of features
 
+print(tt.GREEN + "Number of examples: " + tt.END + f"{M}")
+print(tt.GREEN + "Number of features: " + tt.END + f"{N}")
+
 # Merge examples and labels to shuffle
 # We do this in order to keep each label in the same index of the corresponding example
 Xy_shuffled = np.zeros((M, N + 1), np.uint8)
+print(tt.GREEN + "Merging examples with labels in order to shuffle..." + tt.END)
 for example, label, merge in zip(examples, labels, Xy_shuffled):
     merge[:N] = example
     merge[N] = label
 
+print(tt.GREEN + "Shuffling dataset..." + tt.END)
 np.random.shuffle(Xy_shuffled)
+
+print(tt.GREEN + "Splitting dataset into training and test datasets..." + tt.END)
 
 # Split into training and test set
 training_percentage = 0.7 # The remaining 1 - training_percentage will be the test percentage
@@ -28,6 +42,12 @@ Xy_training, Xy_test = np.split(Xy_shuffled, [int(training_percentage * M)])
 # Separate examples and labels
 X_training = Xy_training[:, : N]
 y_training = Xy_training[:, N]
+m_training = len(X_training)
+
+print(tt.GREEN + "Number of training examples: " + tt.END + f"{m_training}")
 
 X_test = Xy_test[:, : N]
 y_test = Xy_test[:, N]
+m_test = len(X_test)
+
+print(tt.GREEN + "Number of test examples: " + tt.END + f"{m_test}")
