6637d1ed86778e731c2d15b2a1feedc0d1d4c8da
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-08 23:10:44 +0300

    feat: Add constant directories to constants module

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,7 @@ DRAW = 3
 MM_WIN = 10
 MM_DRAW = 0
 MM_LOSS = -10
+GAMES_DIR = "./src/generated/games.npy"
+LABELS_DIR = "./src/generated/labels.npy"
+FEATURES_DIR = "./src/generated/features.npy"
+MODEL_DIR = "./src/generated/3t_model.h5"
