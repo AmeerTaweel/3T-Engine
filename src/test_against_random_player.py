18a94338143796ec04ee953e8159f39d00823bc2
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-09 12:05:46 +0300

    feat: Choose playing type randomly

--- src/test_against_random_player.py
+++ src/test_against_random_player.py
@@ -15,5 +15,9 @@ model = tf.keras.models.load_model(constants.MODEL_DIR)
 G = 10000 # Number of games against random player
 N = constants.BOARD_SIZE
 
+p_types = np.array([(1, 2), (2, 1)], np.uint8)
+
 for i in range(G):
     current_game = np.full(N, constants.EMPTY_CELL, np.uint8)
+    mp_type, rp_type = p_types[np.random.randint(0, 2)] # Model and random player playing types
+    
