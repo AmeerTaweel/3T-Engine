0abcf230b81756cd995fc084ceb598684460aec4
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-09 12:52:55 +0300

    feat: Split code into two functions

--- src/generate_features.py
+++ src/generate_features.py
@@ -2,6 +2,17 @@ import time
 import numpy as np
 from utils import TerminalText as tt
 from utils import getPlayingType
+import constants
+
+def getFeatures(game, b_size):
+    example = np.zeros(b_size, np.uint8)
+    p_type = getPlayingType(game)
+    for i, cell in enumerate(game):
+        if cell == p_type:
+            example[i] = 1
+        elif cell != p_type and cell != constants.EMPTY_CELL:
+            example[i] = 2
+    return example
 
 def generateFeatures(games, M, b_size):
     start_time = time.time() # To measure script running time
@@ -13,12 +24,7 @@ def generateFeatures(games, M, b_size):
     for i, game in enumerate(games):
         percentage = "%.2f" % (((i + 1) / M) * 100) # Limit to two decimal points
         print(tt.ERASE + tt.BLUE + "Converting Games To Machine Learning Ready Features: " + tt.END + f"{percentage}%")
-        p_type = getPlayingType(game)
-        for j, cell in enumerate(game):
-            if cell == p_type:
-                examples[i][j] = 1
-            elif cell != p_type and cell != 0:
-                examples[i][j] = 2
+        examples[i] = getFeatures(game, b_size)
     
     total_time = time.time() - start_time
     print(tt.BLUE + "Generated features for " + tt.END + f"{M}" + tt.BLUE + " games in"
