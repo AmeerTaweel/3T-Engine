d472437d608111574343c79b5e429aa8390d4c6f
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-11 21:20:07 +0300

    feat: Finish converting to JavaScript

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -8,10 +8,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Vue } from 'vue-property-decorator';
-
-export default class App extends Vue {}
+<script>
+export default {
+  name: 'app',
+}
 </script>
 
 <style lang="sass">
