499eed10a1b39cf144484629ccaf848c798dba5b
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-11 20:39:10 +0300

    feat: Set 100% height and prevent scrolling

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" class="w-100 h-100 p-5">
     <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -17,13 +20,22 @@ import HelloWorld from './components/HelloWorld.vue';
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+// Color Variables
+$pickled-bluewood: #2c3e50
+$ocean-green: #42B983
+$white: #ffffff
+
+// Theme
+$primary-color: $ocean-green
+$secondary-color: $pickled-bluewood
+
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: $secondary-color
+  background-color: $primary-color
+  overflow: hidden
 </style>
