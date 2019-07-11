10d193d0e57232fb92d5c406b756bd65b2eff80f
Ameer Taweel <ameertaweel2002@gmail.com>
2019-07-11 20:40:40 +0300

    feat: Empty screen

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <div id="app" class="w-100 h-100 p-5">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
 export default class App extends Vue {}
 </script>
 
